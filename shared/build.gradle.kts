9fdbbb05463566cecad1d93012f3072f4013a5dc
Amolak Singh <amolaksingh@codebrewinnovations.com>
2026-02-13 16:43:32 +0530

    Publish Android AAR correctly

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("multiplatform")
-    id("com.android.library")
+    id("com.android.kotlin.multiplatform.library")
     id("maven-publish")
 }
 
@@ -11,37 +11,35 @@ kotlin {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
+
+        // THIS IS MANDATORY FOR AAR PUBLISHING
+        publishLibraryVariants("release")
     }
-    
-    listOf(
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "Shared"
-            isStatic = true
-        }
-    }
-    
+
+    iosArm64()
+    iosSimulatorArm64()
+
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            // shared code here
         }
         commonTest.dependencies {
-            implementation(libs.kotlin.test)
+            implementation(kotlin("test"))
         }
     }
 }
 
 android {
     namespace = "com.shareutils.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
 
@@ -49,7 +47,6 @@ publishing {
     publications.withType<MavenPublication>().configureEach {
         groupId = "com.github.amolaksingh"
         artifactId = "shareutils"
-        version = "1.0.0"
+        version = "1.0.1" // bump version
     }
 }
-
