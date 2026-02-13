5d875e21f8f0bd6ab06b90da2e4d7e26ee722870
Amolak Singh <amolaksingh@codebrewinnovations.com>
2026-02-13 16:50:52 +0530

    Fix KMP Android publishing using stable setup

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,27 +2,23 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("multiplatform")
-    id("com.android.kotlin.multiplatform.library")
+    id("com.android.library")
     id("maven-publish")
 }
 
 kotlin {
+
     androidTarget {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
-
-        // THIS IS MANDATORY FOR AAR PUBLISHING
-        publishLibraryVariants("release")
     }
 
     iosArm64()
     iosSimulatorArm64()
 
     sourceSets {
-        commonMain.dependencies {
-            // shared code here
-        }
+        commonMain.dependencies { }
         commonTest.dependencies {
             implementation(kotlin("test"))
         }
@@ -47,6 +43,6 @@ publishing {
     publications.withType<MavenPublication>().configureEach {
         groupId = "com.github.amolaksingh"
         artifactId = "shareutils"
-        version = "1.0.1" // bump version
+        version = "1.0.3"
     }
 }
